cd1397ccb64d0c568c78d2d3d300b0babcb10bc4
zhangxhbeta <zhangxhbeta@gmail.com>
2017-05-16 22:45:18 +0800

    调整 postcss，新建一个页面测试

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <mt-header fixed title="标题">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
+
+    <div class="page-toast-wrapper">
+      <mt-button @click.native="openToast" size="large">点击弹出 Toast</mt-button>
+      <mt-button @click.native="openToastWithIcon" size="large">点击弹出带有 icon 的 Toast</mt-button>
+      <mt-button @click.native="openBottomToast" size="large">自定义 Toast 位置</mt-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import { Toast } from 'mint-ui'
+
+  export default {
+    methods: {
+      openToast () {
+        Toast('提示信息')
+      },
+      openToastWithIcon () {
+        Toast({
+          message: '操作成功',
+          iconClass: 'mintui mintui-success'
+        })
+      },
+      openBottomToast () {
+        Toast({
+          message: '提示信息',
+          position: 'bottom'
+        })
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style scoped lang="less">
+  .page-toast-wrapper {
+    padding: 0 20px;
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    transform: translateY(-50%);
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+    button:not(:last-child) {
+      margin-bottom: 20px;
+    }
+  }
 </style>
